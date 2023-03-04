585c134ab82e68381433e1a687e76fb7d08fbb0f
Moje jmeno <lukas.juricek@ideastatica.com>
2023-03-04 13:56:12 +0100

    While_2.cast

--- Cyklus while.py
+++ Cyklus while.py
@@ -1,21 +1,7 @@
-print("hello world")
+l = [1,2,3]
+len(l)
+iter(l)
 
 
-#Obecně while loop
 
-i = 1
-while i < 6:
-    print(i)
-    i = i+1
-    print("hotovo")
 
-index = 0
-
-while index < 20 and len(str(index)) == 2:
-    print(index)
-    index = index + 1
-index = 1
-
-while index > 0:  # vyhodnocené ohlášení má stále hodnotu `True`
-    print(index)
-    index = index + 1
