20ff7669150fd6098bf0a51d1e48fe2fb58459a0
Moje jmeno <lukas.juricek@ideastatica.com>
2023-03-28 18:28:16 +0200

    First commit lesson 11 -pokracovani

--- Lesson 11.py
+++ Lesson 11.py
@@ -1,11 +1,29 @@
 import json
-print("hello","Fellas")
 
 # JSON - java script
-{
+
+chuckuv_slovnik = {
     "jmeno": "Chuck Norris",
     "neuspech": None,
     "kliky": "vsechny",
     "konkurence": False,
+    "fanousek": "Łukasz"
 }
-print("jmeno")
+vystup_s_jsonem = json.dumps(chuckuv_slovnik)
+print(vystup_s_jsonem)
+
+# bez knihovny nemůžeš pracovat s JSON objekty
+import json
+chuckuv_slovnik = {
+    "jmeno": "Chuck Norris",
+    "neuspech": None,
+    "kliky": "vsechny",
+    "konkurence": False,
+    "fanousek": "Łukasz"
+}
+# funkci 'open' nachystáš objekt v Pythonu
+json_soubor = open("prvni_json_soubor.json", mode="w")
+# metoda 'dump' uloží objektu do souboru
+json.dump(chuckuv_slovnik, json_soubor)
+# ... nezapomeň objekt ukončit
+json_soubor.close()
